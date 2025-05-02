45edbdba89423f61bf32de97083e808bcbb5c107
ZM-7 <combiasso57@gmail.com>
2025-05-02 15:27:15 +0100

    updated the premake file
    I updated the file a while ago locally but didnt update the submodule's one, leading to issues when cloning from the main project's repo

--- premake5.lua
+++ premake5.lua
@@ -11,12 +11,13 @@ project "ImGui"
 	"imgui.h",
 	"imgui.cpp",
 	"imgui_draw.cpp",
+	"imgui_demo.cpp",
 	"imgui_internal.h",
+	"imgui_tables.cpp",
 	"imgui_widgets.cpp",
 	"imstb_rectpack.h",
 	"imstb_textedit.h",
 	"imstb_truetype.h",
-	"imgui_demo.cpp"
 	}
 	
 	filter "system:windows"
